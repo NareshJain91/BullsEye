0d844442c653b32e557accff3d21ffd716b96e15
Naresh <naresh.jain@usbank.com>
2020-04-05 01:13:30 +0530

    InitialSetUp

---
+++ BullsEye/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FastlaneDemo
+//
+//  Created by Naresh Jain on 03/04/20.
+//  Copyright © 2020 Naresh Jain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+//      let app = XCUIApplication()
+//      setupSnapshot(app)
+//      app.launch()
+        return true
+  }
+
+
+}
+
